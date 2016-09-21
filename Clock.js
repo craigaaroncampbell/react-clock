1c9452725d4513daef20b38798f2e11c708d4578
Craig <cragaaroncampbell@gmail.com>
2016-09-21 10:42:17 -0700

    initial positioning of numbers on dial

--- Clock.js
+++ Clock.js
@@ -11,7 +11,7 @@ var Clock = React.createClass({
   },
 
   getTime: function() {
-    var time = moment().format('hh:mm:ss')
+    var time = moment().format('hh:mm:ss');
     this.setState({
       time: time
     });
@@ -34,17 +34,17 @@ var Clock = React.createClass({
         </h2>
         <h3>{this.state.time}</h3>
         <div className='clock-face'>
-          <div className="clock-hours" style={{transform: 'rotate(' + ((parseInt(this.state.time.slice(0,2)) - 3 +  parseInt(this.state.time.slice(3,5))/60 + (parseInt(this.state.time.slice(-2))) / 3600) * 30) + 'deg)'}} >
+          <div className="hand hour-hand" style={{transform: 'rotate(' + ((parseInt(this.state.time.slice(0,2)) - 3 +  parseInt(this.state.time.slice(3,5))/60 + (parseInt(this.state.time.slice(-2))) / 3600) * 30) + 'deg)'}} >
           </div>
 
-          <div className="clock-minutes" style={{transform: 'rotate(' + ((parseInt(this.state.time.slice(3,5)) - 15 + (parseInt(this.state.time.slice(-2))) / 60) * 6) + 'deg)'}} >
+          <div className="hand minute-hand" style={{transform: 'rotate(' + ((parseInt(this.state.time.slice(3,5)) - 15 + (parseInt(this.state.time.slice(-2))) / 60) * 6) + 'deg)'}} >
           </div>
 
-          <div className="clock-seconds" style={{transform: 'rotate(' + (parseInt(this.state.time.slice(-2)) - 15)*6 + 'deg)'}} >
+          <div className="hand second-hand" style={{transform: 'rotate(' + (parseInt(this.state.time.slice(-2)) - 15)*6 + 'deg)'}} >
           </div>
 
           {this.props.hours.map(function(hour){
-            return <div key={hour} className={'m'+ hour + ' marker'}>{hour}</div>
+            return <div key={hour} className={'hour'+ hour + ' hour'}><p>{hour}</p></div>
           })}
 
         </div>
